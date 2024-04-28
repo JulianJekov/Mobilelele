ae6846a0e2505e25db77d601ec87003a78147a84
JulianJekov <drmsz@abv.bg>
2024-04-28 17:30:35 +0300

    User entity

---
+++ src/main/java/org/softuni/mobilelele/model/dto/UserLoginDTO.java
@@ -0,0 +1,7 @@
+package org.softuni.mobilelele.model.dto;
+
+public class UserLoginDTO {
+    private String email;
+
+    private String password;
+}
--- src/main/java/org/softuni/mobilelele/web/UserLoginController.java
+++ src/main/java/org/softuni/mobilelele/web/UserLoginController.java
@@ -1,8 +1,14 @@
 package org.softuni.mobilelele.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserLoginController {
-    
+
+    @GetMapping("/users/login")
+    public String login() {
+
+        return "auth-login";
+    }
 }
